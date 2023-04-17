60d84db7b3b4025c98fb222a02835784a7402274
Dana <dana.lalovaod@gmail.com>
2023-04-17 22:28:27 +0300

    feat: 10 hometask solution

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,18 +1,17 @@
+import Notiflix from 'notiflix';
+
 export default function fetchCountries(name) {
-
-  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags.svg,languages`) ///https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags.svg,languages
-
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ) 
     .then(response => {
       if (!response.ok) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
         throw new Error(response.status);
       }
       return response.json();
     })
-    // .then(data => {
-    //     data.map(country => {
-    //         console.log('country.name.official', country, country.name.official);
-    //         // refs.list.innerHTML(`<li>${country.name.official}</li>`);
-    //     })
-    // })
-    .catch(error => console.log(error));
+    .catch(error => {
+      console.log(error);
+    });
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import fetchCountries from './fetchCountries.js';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -12,32 +13,66 @@ const refs = {
 
 refs.query.addEventListener('input', debounce(onInputHandler, DEBOUNCE_DELAY));
 
-let country = '';
+const countriesList = [];
 
 function onInputHandler(event) {
-  console.log(event.target.value);
+  let countryQuery = event.target.value.trim();
+  refs.list.innerHTML = '';
+  refs.info.innerHTML = '';
+  countriesList.length = 0;
 
-  country = event.target.value;
-  const countriesList = [];
-  let countriesToRender = '';
+  if (!countryQuery) {
+    countriesList.length = 0;
+    return;
+  }
 
-
-  fetchCountries(country)
+  fetchCountries(countryQuery)
     .then(data => {
       data.map(country => {
         countriesList.push(country);
-        // console.log('country.name.official', country, country.name.official);
-        console.log('countriesList', countriesList);
-        // renderCountries (country);
-        countriesToRender+=`<li>${country.name.official}</li>`;
-        return countriesToRender;
       });
-    }).then(data => refs.list.insertAdjacentHTML("beforeend" , data))
+
+      renderCountries(countriesList);
+    })
     .catch(error => console.log(error));
 }
 
+function renderCountries(countryArr) {
+  if (countryArr.length === 1) {
+    const languagesObj = countryArr[0].languages;
+    let languagesString = '';
 
-function renderCountries (country){
+    //перебор объекта с языками чтобы вытащить их в строку
+    for (let key in languagesObj) {
+      languagesString += `${languagesObj[key]}, `;
+    }
 
+    let countriesToRender = `<li> <img src="${countryArr[0].flags.svg}" width="32" height="auto"> ${countryArr[0].name.official}</li>`;
+    let descr = `<b>Capital: </b>${countryArr[0].capital}  <br>
+    <b>Population: </b> ${countryArr[0].population}  <br>
+    <b>Languages: </b> ${languagesString}
+    `;
 
+    //рендер заголовка и описания
+    refs.info.insertAdjacentHTML('afterbegin', descr);
+    refs.list.insertAdjacentHTML('beforeend', countriesToRender);
+
+    return;
+  } else if (countryArr.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name'
+    );
+    return;
+  } else if (countryArr.length >= 2 && countryArr.length <= 10) {
+    //рендер разметки со списком стран через reduce
+    let countriesToRender = countryArr.reduce((accum, country) => {
+      console.log(country.flags.svg);
+      return (
+        accum +
+        `<li> <img src="${country.flags.svg}" width="32" height="auto">   ${country.name.official}</li>\n`
+      );
+    }, '');
+
+    return refs.list.insertAdjacentHTML('beforeend', countriesToRender);
+  }
 }
