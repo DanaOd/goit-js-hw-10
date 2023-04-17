9846121dde4b0d751ae834a9e80cc6d2c51cfeb9
DanaOd <dana.lalovaod@gmail.com>
2023-04-17 19:30:50 +0000

    Deploying to gh-pages from @ DanaOd/goit-js-hw-10@60d84db7b3b4025c98fb222a02835784a7402274 🚀

--- index.6a206f6e.js
+++
@@ -1,2 +0,0 @@
-console.log("hi");fetch("https://restcountries.com/v3.1/name/deutschland").then((o=>{if(!o.ok)throw new Error(o.status);return o.json()})).then((o=>{console.log(o)})).catch((o=>console.log(o)));
-//# sourceMappingURL=index.6a206f6e.js.map
--- index.d4af605b.js
+++
@@ -1,2 +0,0 @@
-!function(){console.log("hi");fetch("https://restcountries.com/v3.1/name/deutschland").then((function(n){if(!n.ok)throw new Error(n.status);return n.json()})).then((function(n){console.log(n)})).catch((function(n){return console.log(n)}))}();
-//# sourceMappingURL=index.d4af605b.js.map
